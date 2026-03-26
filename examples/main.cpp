31139ed0946e18e8addb706df01ad3c84040f821
HisameZero <77342033+NoahSch1999@users.noreply.github.com>
2026-03-26 20:22:11 +0100

    Add files via upload

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,40 +1,51 @@
+#ifdef COMPILE_EXAMPLE
 #include <iostream>
 #include <chrono>
 #include "include/aZeroAudio.hpp"
 
-#ifdef COMPILE_EXAMPLE
-#include <Windows.h>
 int main(int argc, char* argv[]) {
 	
+	// Init the audio engine
 	aZero::Audio::AudioEngine audioEngine;
-	aZero::Audio::AudioSource source = audioEngine.CreateSourceFromFile(std::string(AUDIO_SOURCE_PATH) + "drums.wav").value();
 
-	using clock = std::chrono::high_resolution_clock;
-	auto lastTime = clock::now();
-	float angle = 0.f;
-	float speed = 1500.f;
-	float radius = 5.f;
-	audioEngine.SetListenerForward(0, 0, 1);
-	audioEngine.SetListenerPosition(0, 0, 0.f);
-	source.Play();
-	source.Fade(5000, 0, 1);
-	while (source.IsPlaying()) {
-		auto currentTime = clock::now();
-		std::chrono::duration<float> delta = currentTime - lastTime;
-		lastTime = currentTime;
-		float deltaTime = delta.count();
+	// Load a source with streaming enabled
+	std::optional<aZero::Audio::AudioSource> sourceOpt = audioEngine.CreateFromFile(std::string(AUDIO_SOURCE_PATH) + "drums.wav", true);
+	if (sourceOpt.has_value()) { // If the source was loaded
 
-		float x = radius * cos(angle);
-		float y = radius * sin(angle);
-		static bool idk = true;
-		if (angle > 10000 && idk) {
-			idk = false;
-			//source.Seek(0);
-			//source.Fade(5000, -1, 0);
+		// Move, since copy ops are prohibited
+		aZero::Audio::AudioSource source = std::move(sourceOpt.value());
+
+		// Just some stuff for the example
+		using clock = std::chrono::high_resolution_clock;
+		auto lastTime = clock::now();
+		float angle = 0.f;
+		float speed = 1500.f;
+		float radius = 5.f;
+		//
+
+		// Set the listeners forward and position
+		audioEngine.SetListenerForward(0, 0, 1);
+		audioEngine.SetListenerPosition(0, 0, 0.f);
+
+		// Start playing the source
+		source.Play();
+
+		while (source.IsPlaying()) { // Loops as long as the audio is playing
+
+			// Just some stuff for the example
+			auto currentTime = clock::now();
+			std::chrono::duration<float> delta = currentTime - lastTime;
+			lastTime = currentTime;
+			float deltaTime = delta.count();
+			//
+
+			// Rotate the audiosource around origo
+			float x = radius * cos(angle);
+			float y = radius * sin(angle);
+			source.SetWorldPosition(x, y, 0);
+
+			angle += speed * deltaTime;
 		}
-		std::cout << angle << "\n";
-		source.SetWorldPosition(x, y, 0);
-		angle += speed * deltaTime;
 	}
 
 	return 0;
